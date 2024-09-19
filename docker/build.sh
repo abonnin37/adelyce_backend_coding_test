1c22ef1411870c74a955289deb971348a0b54e43
abonnin <bonnin.a.k@gmail.com>
2024-09-19 21:18:14 +0200

    Add user and api authentication

--- docker/build.sh
+++ docker/build.sh
@@ -2,5 +2,7 @@
 
 composer install -n
 bin/console doctrine:migrations:migrate --no-interaction
+bin/console doctrine:fixtures:load --no-interaction
+bin/console lexik:jwt:generate-keypair --skip-if-exists
 
 exec "$@"
